b9bd067752a887fbe2d789c0b171342f3035bbf9
ashish3978 <123528499+ashish3978@users.noreply.github.com>
2024-07-13 19:33:29 +0530

    done

--- New folder (2)/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController');
-
-// Register user route
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-module.exports = router;
